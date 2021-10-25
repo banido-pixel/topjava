6fdd47df6fad83baa35942672e8f8b7c3a5ff435
Grigory <wertyas97@gmail.com>
2021-10-25 21:06:01 +0300

    HW3 implementation

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,9 @@
-DELETE FROM user_roles;
-DELETE FROM users;
+DELETE
+FROM user_roles;
+DELETE
+FROM meals;
+DELETE
+FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -9,3 +13,14 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, date_time, description, calories)
+VALUES (100000, '2020-10-30 09:00', 'Завтрак', 600),
+       (100000, '2020-10-30 14:00', 'Обед', 1200),
+       (100000, '2020-10-30 19:00', 'Ужин', 400),
+       (100000, '2020-11-30 09:00', 'Завтрак', 500),
+       (100000, '2020-11-30 14:00', 'Обед', 1000),
+       (100000, '2020-11-30 19:00', 'Ужин', 400),
+       (100001, '2020-11-30 09:00', 'Завтрак Админ', 500),
+       (100001, '2020-11-30 14:00', 'Обед Админ', 1000),
+       (100001, '2020-11-30 19:00', 'Ужин Адмиин', 400);
